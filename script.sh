2e4be2844395c9d7933ae24f3b6deb8ba2492194
kvnbanunu <kvnbanunu@gmail.com>
2026-02-10 12:31:17 -0800

    script completed

--- script.sh
+++ script.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-echo "COMP 8003 Lab 5: Weaponization"
+echo "COMP 8003 Lab 5: Weaponization\n"
+
+file="$HOME/.$1"
+target="/etc/passwd"
+
+if [[ -e "$file" ]]; then
+    echo "dir $file already exists"
+    exit 1
+fi
+
+mkdir "$file"
+echo "dir $file created"
+
+cp "$target" "$file/passwd"
+echo "$target copied to $file"
+exit 0
